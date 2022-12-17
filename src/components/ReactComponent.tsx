f237c9e8674047fdaa39ed7ca39acc8f245fa50f
Daniel Sintimbrean <ddsintim@inf.upv.es>
2022-12-17 23:12:31 +0100

    debería fallar

--- src/components/ReactComponent.tsx
+++ src/components/ReactComponent.tsx
@@ -12,7 +12,7 @@ export default function ReactComponent() {
       <button
         className="btn btn-primary max-w-md"
         disabled={contador >= 10}
-        onClick={() => setContador((prev) => prev + 1)}
+        onClick={() => setContador((prev) => prev + 2)}
       >
         {contador === 0 ? "Click me" : "Click me again"}
       </button>

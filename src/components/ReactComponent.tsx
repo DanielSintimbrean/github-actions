441018ed833075d18c94229ea4c3390bce9bcf49
Daniel Sintimbrean <ddsintim@inf.upv.es>
2022-12-17 23:22:25 +0100

    --amend

--- src/components/ReactComponent.tsx
+++ src/components/ReactComponent.tsx
@@ -5,8 +5,8 @@ export default function ReactComponent() {
   const [contador, setContador] = useState(0);
 
   return (
-    <div className="flex flex-col max-w-lg mt-9 justify-center items-center">
-      <div className="text-primary-focus text-center mb-5 gap-5">
+    <div className="flex flex-col max-w-lg mt-9 justify-center items-center gap-4">
+      <div className="text-primary-focus text-center mb-5">
         Hello from ReactComponent - {contador}
       </div>
       <button
@@ -14,7 +14,7 @@ export default function ReactComponent() {
         disabled={contador >= 10}
         onClick={() => setContador((prev) => prev + 1)}
       >
-        {contador === 0 ? "Click me" : "Click me again"}
+        {contador === 0 ? "Click me" : "Increment"}
       </button>
       {contador > 0 && (
         <button
